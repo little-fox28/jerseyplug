f507ca293265f891b0febb1e2b095854b9300180
Tran Hung Thinh <124481874+little-fox28@users.noreply.github.com>
2026-07-23 13:55:34 +0700

    feat: add custom checkout form template and generic page template with conditional checkout layout

--- jerseyplug/pages/page.php
+++ jerseyplug/pages/page.php
@@ -28,7 +28,7 @@ get_header();
 							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
 								<path d="m15 18-6-6 6-6" />
 							</svg>
-							<?php esc_html_e('Back to Cart', 'woocommerce'); ?>
+							<?php echo esc_html( jerseyplug_pll( 'Back to Cart' ) ); ?>
 						</a>
 					</div>
 				<?php endif; ?>
